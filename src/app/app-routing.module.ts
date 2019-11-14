444be74ea36c39a1d4e4a6ca09db2c2f8b8f0216
l.janjgava <l.janjgava@fortunejack.com>
2019-11-14 18:23:48 +0400

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HeroComponent } from './hero/hero.component';
+import { XyzComponent } from './xyz/xyz.component';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'hero',
+    component:HeroComponent
+  },
+  {
+    path:'xyz',
+    component:XyzComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { XyzComponent } from './xyz/xyz.component';
+import { HeroComponent } from './hero/hero.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    XyzComponent,
+    HeroComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
